aaf97f15d20d0cac9e5e21762b74aef3b32e97c3
HouJie <houjie_jack@outlook.com>
2020-09-06 21:46:07 +0800

    读文件工具方法

--- server/utils/file_utils.js
+++ server/utils/file_utils.js
@@ -1,10 +1,20 @@
-const fs = require('fs')
+// 读取文件的工具方法
+const fs = require('fs') // 文件模块
 module.exports = async filePath => {
+  // 异步任务→包装为一个Promise对象中
   return new Promise((resolve, reject) => {
-    fs.readFile(filePath, (err, data) => {
+    /**
+     * readFile()参数 
+     * 文件路径
+     * 文件编码
+     * 回调函数
+     */
+    fs.readFile(filePath, 'utf-8', (err, data) => {
       if (err) {
+        // 读取失败时调用Promise的reject
         reject(err)
       } else {
+        // 读取成功时调用Pormise的resolve
         resolve(data)
       }
     })
