50269aa2ec722e663cbb167ff6ea4bfcbd5455d6
HouJie <houjie_jack@outlook.com>
2020-09-07 10:54:23 +0800

    初始化项目，配置echarts

--- client/src/main.js
+++ client/src/main.js
@@ -4,7 +4,13 @@ import router from './router'
 import store from './store'
 import SocketService from '@/utils/socket_service'
 
+// 1.3引入axios，配置基础url，挂载至vue原型
+import axios from 'axios'
+axios.defaults.baseURL = 'http://10.236.163.76:8080/api'
+Vue.prototype.$http = axios
+
 // import echarts from 'echarts'
+// 1.2.2 vue原型上挂载echarts
 Vue.prototype.$echarts = window.echarts
 Vue.config.productionTip = false
 
